b7af8e31a60905f7b66a96a70ec3d26d47e880b1
barry <yangliucool@icloud.com>
2018-03-21 06:47:51 +0800

    fix rc5 S2 bug

--- Src/IR_RC5.c
+++ Src/IR_RC5.c
@@ -8,7 +8,7 @@ void IR_send_RC5(struct IR_RC5_t *IR_RC5)
   IR_set_carrier_freq(RC5_FREQ);
   
   IR_send_bit_data_BI_PHASE(RC5_TX_LSB_FIRST, RC5_BIT1_HIGH_FIRST, RC5_DATA_LEN, 1, 1);
-  IR_send_bit_data_BI_PHASE(RC5_TX_LSB_FIRST, RC5_BIT1_HIGH_FIRST, RC5_DATA_LEN, IR_RC5->IR_command | (1 << 6), 1);
+  IR_send_bit_data_BI_PHASE(RC5_TX_LSB_FIRST, RC5_BIT1_HIGH_FIRST, RC5_DATA_LEN, (IR_RC5->IR_command >> 6) & 1, 1);
   
   if (RC5_toggle)
   {
